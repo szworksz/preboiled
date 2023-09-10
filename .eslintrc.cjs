0c27de8f81ab2d22d82c52d54fdae1621562d5a8
S <wsasza@gmail.com>
2023-09-10 15:47:12 +0200

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+/* eslint-env node */
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: true,
+      tsconfigRootDir: __dirname,
+    },
+    root: true,
+  };
---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import './app.css'
+
+export function App() {
+
+  return (
+    <>
+    <div>
+      <h1>uwu</h1>
+    </div>
+    </>
+  )
+}
